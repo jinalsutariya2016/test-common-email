9ceb0c72099dfdd492429370cbb062b87f00edce
Jinal <jinalsutariya2016@gmail.com>
2021-03-30 13:31:30 -0400

    Created and Test getHostName method successfully

--- src/test/java/org/apache/commons/mail/EmailTest.java
+++ src/test/java/org/apache/commons/mail/EmailTest.java
@@ -149,6 +149,49 @@ public class EmailTest {
 		
 		assertEquals(this.email.subject, "test mail");
 	}
+//	/*******************************************Test getHostName () function 100% done*/
+	@Test
+	public void testGetHostName() {
+		
+		email.setHostName("Localhost");
+		String hostName = email.getHostName();
+		assertEquals("Localhost", hostName);
+		
+	}
+	@Test
+	public void testGetSetHostNull() {
+		email.setHostName(null);
+		assertEquals(null, email.getHostName());
+		
+	}
+		
+	@Test
+	public void testGetSetHostNameWithSession() {
+		Properties properties = new Properties();
+		Session session = Session.getDefaultInstance(properties,null);
+		properties.put(EmailConstants.MAIL_HOST,"Localhost");
+		email.setHostName("Localhost");
+		assertEquals("Localhost", email.getHostName());
+		
+		
+	}
+	
+	@Test
+	public void testGetHostName_shouldSetMailHost_whenSessionIsNotNull() {
+		final Properties properties = new Properties(System.getProperties());
+		properties.setProperty(EmailConstants.MAIL_TRANSPORT_PROTOCOL, EmailConstants.SMTP);
+		properties.setProperty(
+		EmailConstants.MAIL_PORT, String.valueOf(587));
+		properties.setProperty(EmailConstants.MAIL_HOST, "localhost");
+		properties.setProperty(EmailConstants.MAIL_DEBUG, String.valueOf(false));
+		  
+		final Session mySession = Session.getInstance(properties, null);
+		this.email.setMailSession(mySession);
+		
+		String hostName = this.email.getHostName();
+		
+		assertEquals(hostName, "localhost");
+	}
 	
 
 	
